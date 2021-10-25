ee2fb0ff9c45f0a7b8d1fa47952cefc02321a138
cfhj8520 <cfhj8520@naver.com>
2021-10-25 21:19:03 +0900

    DAO 작성

--- src/com/fastcampus/biz/board/BoardDAO.java
+++ src/com/fastcampus/biz/board/BoardDAO.java
@@ -34,15 +34,54 @@ public class BoardDAO {
 	}
 	
 	public void updateBoard(String title, String content, int seq) {
-		
+		try {
+			conn = JDBCUtil.getConnection();
+			stmt= conn.prepareStatement(BOARD_UPDATE);
+			stmt.setString(1, title);
+			stmt.setString(2, content);
+			stmt.setInt(3, seq);
+		}catch(Exception e) {
+			e.printStackTrace();
+		}finally {
+			JDBCUtil.close(stmt, conn);
+		}
 	}
 
 	public void deleteBoard(int seq) {
-	
+		try {
+			conn = JDBCUtil.getConnection();
+			stmt= conn.prepareStatement(BOARD_DELETE);
+			stmt.setInt(1, seq);
+			int count = stmt.executeUpdate();
+			System.out.println(count + "건의 데이터 처리 성공!!!");
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}finally {
+			JDBCUtil.close(stmt, conn);
+		}
 	}
 	
 	public void getBoard(int seq) {
-		
+		try {
+			conn = JDBCUtil.getConnection();
+			stmt = conn.prepareStatement(BOARD_GET);
+			stmt.setInt(1, seq);
+			rs = stmt.executeQuery();
+			if(rs.next()) {
+				System.out.println("번호 : " + rs.getInt("SEQ"));
+				System.out.println("제목 : " + rs.getString("TITLE"));
+				System.out.println("작성자 : " + rs.getString("WRITER"));
+				System.out.println("내용 : " + rs.getString("CONTENT"));
+				System.out.println("등록일 : " + rs.getDate("REGDATE"));
+				System.out.println("조회수 : " + rs.getInt("CNT"));
+			}else {
+				System.out.println(seq + "번 게시글은 존재하지 않습니다.");
+			}
+		}catch(Exception e) {
+			e.printStackTrace();
+		}finally {
+			JDBCUtil.close(rs, stmt, conn);
+		}
 	}
 	
 	public void getBoardList() {
