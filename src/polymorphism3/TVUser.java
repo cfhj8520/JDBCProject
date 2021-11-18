2ddf5a34232cc5642930dc7367ef265ed3777045
cfhj8520 <cfhj8520@naver.com>
2021-11-18 22:17:07 +0900

    필요없는 주석 제거

--- src/polymorphism3/TVUser.java
+++ src/polymorphism3/TVUser.java
@@ -3,7 +3,6 @@ package polymorphism3;
 public class TVUser {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		TV tv = TVFactory.getTV(args[0]);
 		tv.powerOn();
 		tv.volumeUp();
